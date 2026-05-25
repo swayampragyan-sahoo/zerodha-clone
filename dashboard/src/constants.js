c295f489f71a7443fa68fac04ed85c8746a129c8
swayampragyan sahoo <swayampragyan0512@gmail.com>
2026-05-25 17:06:02 +0530

    Fixed deployed URL

--- dashboard/src/constants.js
+++ dashboard/src/constants.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL =
-  "https://zerodha-clone-b96g5568i-swayampragyan-sahoo-s-projects.vercel.app";
+  "https://zerodha-clone-bm65de4v7-swayampragyan-sahoo-s-projects.vercel.app";
 
 export const FRONTEND_URL = 
-  "https://zerodha-clone-vr7m.vercel.app/";
+  "https://zerodha-clone-vr7m.vercel.app";
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL =
-  "https://zerodha-clone-b96g5568i-swayampragyan-sahoo-s-projects.vercel.app";
+  "https://zerodha-clone-bm65de4v7-swayampragyan-sahoo-s-projects.vercel.app";
 
 export const DASHBOARD_URL =
   "https://zerodha-clone-ksv6.vercel.app";
